bd34c8227ff534a44c05f15012f8411a0c398aa8
mahmudkhalifa <mahmoudkh@silverkeytech.com>
2020-07-11 13:25:12 +0200

    Add db context

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,6 +12,17 @@ namespace Persistence
             
         }
 
-        public DbSet<Value> Values { get; set; }    
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Value>().HasData(
+                new Value() {Id = 1, Name = "khalifa1"},
+                new Value() {Id = 2, Name = "khalifa2"},
+                new Value() {Id = 3, Name = "khalifa3"});
+        }
     }
+
 }
