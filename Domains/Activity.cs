b7414ebe050b4340c1fc6f6a196a81687c215762
mahmudkhalifa <mahmoudkh@silverkeytech.com>
2020-07-16 13:38:06 +0200

    Add activivty class

--- Domains/Activity.cs
+++ Domains/Activity.cs
@@ -7,10 +7,10 @@ namespace Domains
     public class Activity
     {
         public Guid Id { get; set; }
-
         public string Title { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
+        public string City { get; set; }
         public string Venue { get; set; }
         public DateTime Date { get; set; }
     }
