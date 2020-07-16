56492b7b072e8c292edb5c5bb426114a90644a17
mahmudkhalifa <mahmoudkh@silverkeytech.com>
2020-07-16 13:37:00 +0200

    Update persistence seeded data

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -13,15 +13,17 @@ namespace Persistence
         }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Value>().HasData(
-                new Value() {Id = 1, Name = "khalifa1"},
-                new Value() {Id = 2, Name = "khalifa2"},
-                new Value() {Id = 3, Name = "khalifa3"});
+                new Value() { Id = 1, Name = "khalifa1" },
+                new Value() { Id = 2, Name = "khalifa2" },
+                new Value() { Id = 3, Name = "khalifa3" });
+
         }
     }
 
