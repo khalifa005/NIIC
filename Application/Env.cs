cefb59d30c4a6af14e246165734d94966ef5bd3f
mahmudkhalifa <mahmoudkh@silverkeytech.com>
2020-08-06 04:47:44 +0200

    Add env test

--- Application/Env.cs
+++ Application/Env.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 using System.Threading;
+using Microsoft.AspNetCore.Builder;
 
 namespace Application
 {
     public static class Env
     {
+        public static void GetSetCultureInDifferentWays()
+        {
+            var test = Thread.CurrentThread.CurrentCulture.Name;
+            var test2 = System.Globalization.CultureInfo.CurrentCulture.Name;
+            var test3 = Thread.CurrentThread.CurrentUICulture = new CultureInfo("ar-EG");
+            var test4 = Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("ar-EG");
+            var test5 = Thread.CurrentThread.CurrentCulture.Name;
+
+            // var requestCulture = HttpContext.Features.Get<IRequestCultureFeature>().RequestCulture;
+            //var test=requestCulture.Culture.Name;
+            var test6 = System.Globalization.CultureInfo.CurrentCulture.Name;
+        }
+
         public static bool IsArabic(string lang)
             => lang == "ar";
 
