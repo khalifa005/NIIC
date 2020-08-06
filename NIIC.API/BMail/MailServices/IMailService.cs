84c6483ec0b2311f1929254c0bf88226fb8897bd
mahmudkhalifa <mahmoudkh@silverkeytech.com>
2020-08-06 04:49:11 +0200

    Add send mail

---
+++ NIIC.API/BMail/MailServices/IMailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domains.BMail;
+
+namespace NIIC.API.BMail.MailServices
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+        Task SendWelcomeEmailAsync(WelcomeRequest request);
+    }
+}
