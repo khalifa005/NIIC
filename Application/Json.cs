546194159a70f20da70cfa1110761b116cca1d9c
mahmudkhalifa <mahmoudkh@silverkeytech.com>
2021-01-02 21:08:01 +0200

    Add extra packages

--- Application/Json.cs
+++ Application/Json.cs
@@ -9,116 +9,117 @@ using Utf8Json.Resolvers;
 
 namespace Application
 {
-    //public static class Json
-    //{
-    //    public static readonly IJsonFormatterResolver Standard = StandardResolver.ExcludeNullCamelCase;
-    //    public static readonly IJsonFormatterResolver SnakeCase = StandardResolver.SnakeCase;
+    public static class Json
+    {
+        public static readonly IJsonFormatterResolver Standard = StandardResolver.ExcludeNullCamelCase;
+        public static readonly IJsonFormatterResolver SnakeCase = StandardResolver.SnakeCase;
 
-    //    public static readonly IJsonFormatterResolver AllowPrivate = StandardResolver.AllowPrivateExcludeNullCamelCase;
+        public static readonly IJsonFormatterResolver AllowPrivate = StandardResolver.AllowPrivateExcludeNullCamelCase;
 
-    //    public static string Serialize(object obj)
-    //    {
-    //        return JsonSerializer.ToJsonString(obj, Standard);
-    //    }
+        public static string Serialize(object obj)
+        {
+            return JsonSerializer.ToJsonString(obj, Standard);
+        }
 
-    //    public static string SerializeSnakeCase(object obj)
-    //    {
-    //        return JsonSerializer.ToJsonString(obj, SnakeCase);
-    //    }
+        public static string SerializeSnakeCase(object obj)
+        {
+            return JsonSerializer.ToJsonString(obj, SnakeCase);
+        }
 
-    //    public static byte[] SerializeToBytes(object obj)
-    //    {
-    //        return JsonSerializer.Serialize(obj);
-    //    }
+        public static byte[] SerializeToBytes(object obj)
+        {
+            return JsonSerializer.Serialize(obj);
+        }
 
-    //    public static Task SerializeAsync(Stream str, object obj)
-    //    {
-    //        return JsonSerializer.SerializeAsync(str, obj, Standard);
-    //    }
+        public static Task SerializeAsync(Stream str, object obj)
+        {
+            return JsonSerializer.SerializeAsync(str, obj, Standard);
+        }
 
-    //    public static Task SerializePrivateAsync(Stream str, object obj)
-    //    {
-    //        return JsonSerializer.SerializeAsync(str, obj, AllowPrivate);
-    //    }
+        public static Task SerializePrivateAsync(Stream str, object obj)
+        {
+            return JsonSerializer.SerializeAsync(str, obj, AllowPrivate);
+        }
 
-    //    /// <summary>
-    //    /// Use this as the point of last resort. There are Stream and byte[] options. There's no need
-    //    /// to convert everything to string.
-    //    /// </summary>
-    //    /// <typeparam name="T"></typeparam>
-    //    /// <param name="str"></param>
-    //    /// <returns></returns>
-    //    public static T Deserialize<T>(string str)
-    //    {
-    //        return JsonSerializer.Deserialize<T>(str, Standard);
-    //    }
+        /// <summary>
+        /// Use this as the point of last resort. There are Stream and byte[] options. There's no need
+        /// to convert everything to string.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static T Deserialize<T>(string str)
+        {
+            return JsonSerializer.Deserialize<T>(str, Standard);
+        }
 
-    //    public static T Deserialize<T>(Stream st)
-    //    {
-    //        return JsonSerializer.Deserialize<T>(st, Standard);
-    //    }
+        public static T Deserialize<T>(Stream st)
+        {
+            return JsonSerializer.Deserialize<T>(st, Standard);
+        }
 
-    //    public static T Deserialize<T>(byte[] bytes)
-    //    {
-    //        return JsonSerializer.Deserialize<T>(bytes, Standard);
-    //    }
+        public static T Deserialize<T>(byte[] bytes)
+        {
+            return JsonSerializer.Deserialize<T>(bytes, Standard);
+        }
 
-    //    public static T DeserializePrivate<T>(byte[] bytes)
-    //    {
-    //        return JsonSerializer.Deserialize<T>(bytes, AllowPrivate);
-    //    }
+        public static T DeserializePrivate<T>(byte[] bytes)
+        {
+            return JsonSerializer.Deserialize<T>(bytes, AllowPrivate);
+        }
 
-    //    public static Task<T> DeserializeAsync<T>(Stream st)
-    //    {
-    //        return JsonSerializer.DeserializeAsync<T>(st, Standard);
-    //    }
+        public static Task<T> DeserializeAsync<T>(Stream st)
+        {
+            return JsonSerializer.DeserializeAsync<T>(st, Standard);
+        }
 
-    //    public static M DeserializeFilter<M>(string json)
-    //    {
-    //        var unescaped = Compression.Decompress(Uri.UnescapeDataString(json));
+        public static M DeserializeFilter<M>(string json)
+        {
+            var unescaped = Compression.Decompress(Uri.UnescapeDataString(json));
 
-    //        using (var input = new StringReader(unescaped))
-    //        {
-    //            return Deserialize<M>(input.ReadToEnd());
-    //        }
-    //    }
-    //}
+            using (var input = new StringReader(unescaped))
+            {
+                return Deserialize<M>(input.ReadToEnd());
+            }
+        }
+    }
 
-    //internal static class Compression
-    //{
-    //    public static string Compress(string s)
-    //    {
-    //        var bytes = Encoding.Unicode.GetBytes(s);
-    //        using (var msi = new MemoryStream(bytes))
-    //        {
-    //            using (var mso = new MemoryStream())
-    //            {
-    //                using (var gs = new GZipStream(mso, CompressionMode.Compress))
-    //                {
-    //                    msi.CopyTo(gs);
-    //                }
+    internal static class Compression
+    {
+        public static string Compress(string s)
+        {
+            var bytes = Encoding.Unicode.GetBytes(s);
+            using (var msi = new MemoryStream(bytes))
+            {
+                using (var mso = new MemoryStream())
+                {
+                    using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                    {
+                        msi.CopyTo(gs);
+                    }
 
-    //                return Convert.ToBase64String(mso.ToArray());
-    //            }
-    //        }
-    //    }
+                    return Convert.ToBase64String(mso.ToArray());
+                }
+            }
+        }
 
-    //    public static string Decompress(string s)
-    //    {
-    //        var bytes = Convert.FromBase64String(s);
-    //        using (var msi = new MemoryStream(bytes))
-    //        {
-    //            using (var mso = new MemoryStream())
-    //            {
-    //                using (var gs = new GZipStream(msi, CompressionMode.Decompress))
-    //                {
-    //                    gs.CopyTo(mso);
-    //                }
+        public static string Decompress(string s)
+        {
+            var bytes = Convert.FromBase64String(s);
+            using (var msi = new MemoryStream(bytes))
+            {
+                using (var mso = new MemoryStream())
+                {
+                    using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                    {
+                        gs.CopyTo(mso);
+                    }
 
-    //                return Encoding.Unicode.GetString(mso.ToArray());
-    //            }
-    //        }
-    //    }
-    //}
+                    return Encoding.Unicode.GetString(mso.ToArray());
+                }
+            }
+        }
+    }
+
 }
 
---
+++ Application/LookupNames.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application
+{
+    
+    //public record LookupNames
+    //(
+    //bool City = false,
+    //bool CommentType = false,
+    //bool ComplaintReason = false,
+    //bool EmploymentCenter = false,
+    //bool Governorate = false,
+    //bool InternalPosition = false,
+    //bool InternalRole = false,
+    //bool JobSeekerStatusActive = false,
+    //bool Neighbourhood = false,
+    //bool OrganizationStatusActive = false,
+    //bool Position = false,
+    //bool Role = false,
+    //bool SystemEvent = false,
+    //bool UserContact = false,
+    //bool ComplaintStatus = false
+    //);
+}
